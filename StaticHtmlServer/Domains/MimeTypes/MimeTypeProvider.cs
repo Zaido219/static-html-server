17db23132652eb06f57a5ecaabb93b568cfa2b44
Zaido219 <jpmalbas4@gmail.com>
2026-07-14 23:49:03 +0800

    feat:Mimetype provider class

--- StaticHtmlServer/Domains/MimeTypes/MimeTypeProvider.cs
+++ StaticHtmlServer/Domains/MimeTypes/MimeTypeProvider.cs
@@ -1,10 +1,19 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.StaticFiles;
+
 namespace StaticHtmlServer.Domains.MimeTypes
 {
     public class MimeTypeProvider : IMimeTypeProvider
     {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider = new FileExtensionContentTypeProvider();
         public string GetMimeType(string pathOrFileName)
         {
-            return "";
+            if (_fileExtensionContentTypeProvider.TryGetContentType(pathOrFileName, out string contentType))
+            {
+                return contentType;
+            }
+            // fallback if not supported
+            return "application/octet-stream";
         }
     }
 }
