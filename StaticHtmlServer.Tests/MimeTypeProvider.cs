15babb587aded744c733a69c02b57b7cd98c0388
Zaido219 <jpmalbas4@gmail.com>
2026-07-15 14:19:22 +0800

    feat:Just saving progress

---
+++ StaticHtmlServer.Tests/MimeTypeProvider.cs
@@ -0,0 +1,25 @@
+using System.Reflection.Metadata;
+using StaticHtmlServer.Domains.MimeTypes;
+using Xunit.Sdk;
+
+namespace StaticHtmlServer.Tests
+{
+    public class MimeTypeProviderTests
+    {
+        [Theory]
+        [InlineData("index.html", "text/html")]
+        [InlineData("styles.css", "text/css")]
+        [InlineData("app.js", "application/javascript")]
+        [InlineData("logo.png", "image/png")]
+        
+        public void GetMimeType_WithKnownExtensions_ReturnCorrectMimeType(string fileName, string expectedMimeType)
+        {
+            //given
+            IMimeTypeProvider testProvider = new MimeTypeProvider();
+            // when
+            string result = testProvider.GetMimeType(fileName);
+            //then
+            Assert.Equal(expectedMimeType, result);
+        }
+    }
+}
